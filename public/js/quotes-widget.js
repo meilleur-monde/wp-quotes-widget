f3b48de404afa98791debc94bbf955d810e431ea
Francois Chastanet <fchastanet@gmail.com>
2017-08-29 00:16:20 +0200

    eslint fixes

--- public/js/quotes-widget.js
+++ public/js/quotes-widget.js
@@ -1,37 +1,56 @@
 var betterWorld = betterWorld || {};
 betterWorld.quotes = betterWorld.quotes || {};
-betterWorld.quotes.refresh = function (args) {
-    if (args.ajaxRefresh && !args.autoRefresh) {
-        jQuery("#" + args.instanceID + " .nav-next").html(quotescollectionAjax.loading);
+betterWorld.quotes.refresh = function( args ) {
+    var $widgetDiv = jQuery( '#' + args.instanceID );
+    if ( args.ajaxRefresh && ! args.autoRefresh ) {
+        $widgetDiv.find( '.nav-next' ).html( quotescollectionAjax.loading );
         jQuery.ajax({
-            type: "POST",
+            type: 'POST',
             url: quotescollectionAjax.ajaxUrl,
-            data: "action=quotescollection&_ajax_nonce=" + quotescollectionAjax.nonce + "&current=" + args.currQuoteID + "&char_limit=" + args.charLimit + "&tags=" + args.tags + "&orderby=" + args.orderBy,
-            success: function (response) {
-                if (response === '-1' || !response) {
-                    if (args.ajaxRefresh && args.autoRefresh)
-                        quotescollectionTimer(args);
-                    else if (args.ajaxRefresh && !args.autoRefresh)
-                        jQuery("#" + args.instanceID + " .nav-next").html('<a class=\"next-quote-link\" style=\"cursor:pointer;\" onclick=\"quotescollectionRefreshInstance(\'' + args.instanceID + '\')\">' + quotescollectionAjax.nextQuote + '</a>');
-                }
-                else {
-                    if (args.dynamicFetch) {
+            data:
+                'action=quotescollection&_ajax_nonce=' + quotescollectionAjax.nonce +
+                '&current=' + args.currQuoteID +
+                '&char_limit=' + args.charLimit +
+                '&tags=' + args.tags +
+                '&orderby=' + args.orderBy,
+            success: function( response ) {
+                if ( '-1' === response || ! response ) {
+                    if ( args.ajaxRefresh && args.autoRefresh ) {
+                        quotescollectionTimer( args );
+                    } else if ( args.ajaxRefresh && ! args.autoRefresh ) {
+                        $widgetDiv.find( '.nav-next' ).html(
+                            '<a class="next-quote-link" style="cursor:pointer;" onclick="quotescollectionRefreshInstance(\'' +
+                            args.instanceID +
+                            '\');">' +
+                            quotescollectionAjax.nextQuote +
+                            '</a>'
+                        );
+                    }
+                } else {
+                    if ( args.dynamicFetch ) {
                         args.dynamicFetch = 0;
                     }
                     args.currQuoteID = response.quote_id;
                     quotescollectionInstances[args.instanceID] = args;
-                    display = quotescollectionDisplayFormat(response, args);
-                    jQuery("#" + args.instanceID).hide();
-                    jQuery("#" + args.instanceID).html(display, args);
-                    jQuery("#" + args.instanceID).fadeIn('slow');
-                    if (args.ajaxRefresh && args.autoRefresh)
-                        quotescollectionTimer(args);
+                    display = quotescollectionDisplayFormat( response, args );
+                    $widgetDiv.hide();
+                    $widgetDiv.html( display, args );
+                    $widgetDiv.fadeIn( 'slow' );
+                    if ( args.ajaxRefresh && args.autoRefresh ) {
+                        quotescollectionTimer( args );
+                    }
                 }
             },
-            error: function (xhr, textStatus, errorThrown) {
-                console.log(textStatus + ' ' + xhr.status + ': ' + errorThrown);
-                if (args.ajaxRefresh && !args.autoRefresh) {
-                    jQuery("#" + args.instanceID + " .nav-next").html('<a class=\"next-quote-link\" style=\"cursor:pointer;\" onclick=\"quotescollectionRefreshInstance(\'' + args.instanceID + '\')\">' + quotescollectionAjax.nextQuote + ';</a>');
+            error: function( xhr, textStatus, errorThrown ) {
+                console.log( textStatus + ' ' + xhr.status + ': ' + errorThrown );
+                if ( args.ajaxRefresh && ! args.autoRefresh ) {
+                    $widgetDiv.find( '.nav-next' ).html(
+                        '<a class="next-quote-link" style="cursor:pointer;" onclick="quotescollectionRefreshInstance(\'' +
+                        args.instanceID +
+                        '\');">' +
+                        quotescollectionAjax.nextQuote +
+                        '</a>'
+                    );
                 }
             }
         });
