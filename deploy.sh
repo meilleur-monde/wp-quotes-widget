ec3b4ad93d1b760f23e1a45e02c038f23bd0a947
Francois Chastanet <fchastanet@gmail.com>
2017-08-31 00:16:36 +0200

    travis deploy script removed sha1 file

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
+echo "create deploy temp directory"
 rm -Rf /tmp/better-world-quotes-widget
 mkdir -p /tmp/better-world-quotes-widget
 
-# copy all project files
+echo "copy all project files"
 cp -R admin languages public *.php *.md /tmp/better-world-quotes-widget
 
 # TODO minify the css and js files
 
-# add index.php Silence is golden in each directory where there isn't an index.php
+echo "add index.php Silence is golden in each directory where there isn't an index.php"
 echo "<?php // Silence is golden" > /tmp/index.php
 find /tmp/better-world-quotes-widget -type d \! -exec test -e '{}/index.php' \; -exec cp /tmp/index.php {} \;
 
-# zip the files
+echo "zip the files"
 zip -r /tmp/better-world-quotes-widget.zip /tmp/better-world-quotes-widget
-
-# create checksum file
-sha1sum /tmp/better-world-quotes-widget.zip > /tmp/better-world-quotes-widget.zip.sha1
